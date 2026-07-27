2c6c90fac356462f51fe213cf93d7af438d0fea6
bluoculus <bluoculus@gmail.com>
2026-07-27 15:04:50 +0900

    first

---
+++ src/main/java/com/example/bookwithticket/BookwithticketApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookwithticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwithticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwithticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookwithticket/BookwithticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookwithticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookwithticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
