f190471d51614b21958ba6c6afd3023ae1ac26ba
jeyoon <jypahn@naver.com>
2026-07-31 17:07:31 +0900

    feat: 주문 화면 조회

---
+++ src/main/java/com/example/bookwithticket/order/dto/OrderPageItemDto.java
@@ -0,0 +1,56 @@
+package com.example.bookwithticket.order.dto;
+
+import com.example.bookwithticket.order.entity.BookOrderItemEntity;
+
+public class OrderPageItemDto {
+
+    private final Long orderItemId;
+    private final Long bookId;
+
+    private final String bookTitle;
+    private final String thumbnailUrl;
+
+    private final int price;
+    private final int quantity;
+    private final int totalPrice;
+
+    public OrderPageItemDto(BookOrderItemEntity orderItem) {
+        this.orderItemId = orderItem.getId();
+        this.bookId = orderItem.getBook().getId();
+
+        this.bookTitle = orderItem.getBookTitleSnapshot();
+        this.thumbnailUrl = orderItem.getBook().getThumbnailUrl();
+
+        this.price = orderItem.getPriceSnapshot();
+        this.quantity = orderItem.getQuantity();
+        this.totalPrice = orderItem.getPriceSnapshot() * orderItem.getQuantity();
+    }
+
+    public Long getOrderItemId() {
+        return orderItemId;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+}
