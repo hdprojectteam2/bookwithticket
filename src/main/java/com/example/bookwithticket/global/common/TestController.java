075b51d082134c422d53e16259cf6b8b73a7e12a
jeyoon <jypahn@naver.com>
2026-08-08 14:06:57 +0900

    충돌 해결용

--- src/main/java/com/example/bookwithticket/global/common/TestController.java
+++ src/main/java/com/example/bookwithticket/global/common/TestController.java
@@ -8,14 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-<<<<<<< HEAD
     @GetMapping("/")
     public String root() {
         return "cartTest";
-=======
-    @GetMapping("/api/health")
-    public String healthCheck() {
-        return "Running Successfully";
->>>>>>> 2b355d0dcf6e30160bfe72f0783497efbcf5d7c1
     }
 }
