a6b47dd795c1219596025b6e3cb659d2d1176f86
jeyoon <jypahn@naver.com>
2026-08-11 09:12:57 +0900

    장바구니 entity 최신화, gradle 최신화

--- src/main/java/com/example/bookwithticket/cart/service/PerformanceCartServiceImpl.java
+++ src/main/java/com/example/bookwithticket/cart/service/PerformanceCartServiceImpl.java
@@ -11,8 +11,8 @@ import com.example.bookwithticket.cart.entity.CartEntity;
 import com.example.bookwithticket.cart.entity.PerformanceCartItemEntity;
 import com.example.bookwithticket.cart.repository.CartRepository;
 import com.example.bookwithticket.cart.repository.PerformanceCartItemRepository;
-import com.example.bookwithticket.performance.entity.PerformanceScheduleEntity;
-import com.example.bookwithticket.performance.repository.PerformanceScheduleRepository;
+import com.example.bookwithticket.domain.performance.PerformanceSchedule;
+import com.example.bookwithticket.domain.performance.PerformanceScheduleRepository;
 
 @Service
 @Transactional
@@ -41,14 +41,13 @@ public class PerformanceCartServiceImpl
             throw new IllegalArgumentException("공연 회차 정보가 없습니다.");
         }
 
-        PerformanceScheduleEntity schedule =
+        PerformanceSchedule schedule =
                 performanceScheduleRepository
                         .findById(performanceScheduleId)
                         .orElseThrow(() ->
                                 new IllegalArgumentException("존재하지 않는 공연 회차입니다.")
                         );
 
-        /* 예매 가능 공연 장바구니 예외 처리*/
         if (!LocalDateTime.now().isBefore(
                 schedule.getPerformanceTime()
         )) {
